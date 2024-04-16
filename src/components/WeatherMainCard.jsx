4f588d808c6a8486577e93f6cd42184b871fc66e
Ajitam99 <susacmatija@gmail.com>
2024-04-16 12:44:35 +0200

    changes on the main card

--- src/components/WeatherMainCard.jsx
+++ src/components/WeatherMainCard.jsx
@@ -1,16 +1,35 @@
 import styled from "styled-components";
+import { FaCloudSun } from "react-icons/fa";
 
-const StyledWeatherMainCard = styled.main`
+const Container = styled.main`
   border: 1px solid #ccc;
   border-radius: 10px;
   padding: 20px;
   display: flex;
   flex-direction: row;
+  gap: 48px;
   align-items: center;
+  background-color: rgb(0, 0, 255, 0.3);
+  width: 60%;
+`;
+
+const InfoContainer = styled.main`
+  display: flex;
+  flex-direction: column;
 `;
 
 function WeatherMainCard() {
-  return <StyledWeatherMainCard>Nesto</StyledWeatherMainCard>;
+  return (
+    <Container>
+      <FaCloudSun />
+      <InfoContainer>
+        <p>When</p>
+        <p>City</p>
+        <p>Temperature: XXX</p>
+        <p>Weather</p>
+      </InfoContainer>
+    </Container>
+  );
 }
 
 export default WeatherMainCard;
